d72b8c0c569a73f274c18233016fd42c8342a937
Devaa101 <154732909+Devaa101@users.noreply.github.com>
2025-08-12 14:43:07 -0400

    Update setup_sniffer.sh

--- network-sniff/setup_sniffer.sh
+++ network-sniff/setup_sniffer.sh
@@ -3,21 +3,11 @@
 # Exit on errors
 set -e
 
-# step 1: Create virtual environment if not exists
-if [ ! -d "venv" ]; then
-        echo "[*] Creating Python virtual environment..."
-        Python3 -m venv myvenv
-fi
-
-# step 2: Activate the virtual environment
-echo "[*] Activating virtual environment..."
-source myvenv/bin/activate
-
-# step 3: Install scapy
+# step 1: Install scapy
 echo "[*] Installing Scapy..."
 pip install --upgrade pip
 pip install scapy
 
-# step 4: Run Sniffer.py
+# step 2: Run Sniffer.py
 echo "[*] Running network sniffer..."
 sudo python3 sniffe.py
